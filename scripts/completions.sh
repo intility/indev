ac67b59debc334fcb43faabe87604356b9a10c91
Emil Kjelsrud <emil.kjelsrud@intility.no>
2024-04-23 08:44:42 +0200

    ci: reproducable builds lets go!

--- scripts/completions.sh
+++ scripts/completions.sh
@@ -5,5 +5,6 @@ rm -rf completions
 mkdir completions
 for sh in bash zsh fish; do
 	go run main.go completion "$sh" >"completions/minctl.$sh"
+	# set static accessed and modified date to files
+	touch -a -m -t 202401010000.00 "completions/minctl.$sh"
 done
-sleep 1
