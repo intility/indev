d532b65a502c4d1e950fd9f0cde723317c628905
Callum Powell <callum.powell@intility.no>
2025-12-16 14:21:25 +0100

    fix: make team and cluster get commands case-insensitive

--- pkg/commands/cluster/get.go
+++ pkg/commands/cluster/get.go
@@ -49,7 +49,7 @@ func NewGetCommand(set clientset.ClientSet) *cobra.Command {
 			// Find the cluster with the matching name
 			var cluster *client.Cluster
 			for _, c := range clusters {
-				if c.Name == clusterName {
+				if strings.EqualFold(c.Name, clusterName) {
 					cluster = &c
 					break
 				}
