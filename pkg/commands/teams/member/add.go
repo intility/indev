267d8b840999d034c89fe3b43d3d5d2260bb5cc3
Callum Powell <callum.powell@intility.no>
2025-12-16 10:23:52 +0100

    refactor: extract shared functions to helpers file

--- pkg/commands/teams/member/add.go
+++ pkg/commands/teams/member/add.go
@@ -1,7 +1,6 @@
 package member
 
 import (
-	"context"
 	"fmt"
 	"strings"
 
@@ -123,46 +122,4 @@ func validateOptions(options AddMemberOptions) error {
 	}
 
 	return nil
-}
-
-func getTeamIdByName(ctx context.Context, set clientset.ClientSet, teamName string) (string, error) {
-	teams, err := set.PlatformClient.ListTeams(ctx)
-	if err != nil {
-		return "", redact.Errorf("could not list teams: %w", redact.Safe(err))
-	}
-
-	var team *client.Team
-	for _, t := range teams {
-		if strings.EqualFold(t.Name, teamName) {
-			team = &t
-			break
-		}
-	}
-
-	if team == nil {
-		return "", redact.Errorf("team not found: %s", teamName)
-	}
-
-	return team.ID, nil
-}
-
-func getUserIdByUpn(ctx context.Context, set clientset.ClientSet, upn string) (string, error) {
-	users, err := set.PlatformClient.ListUsers(ctx)
-	if err != nil {
-		return "", redact.Errorf("could not list users: %w", redact.Safe(err))
-	}
-
-	var user *client.User
-	for _, u := range users {
-		if strings.EqualFold(u.UPN, upn) {
-			user = &u
-			break
-		}
-	}
-
-	if user == nil {
-		return "", redact.Errorf("user not found: %s", upn)
-	}
-
-	return user.ID, nil
 }
